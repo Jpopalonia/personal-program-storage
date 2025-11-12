7d1ac5542ae077825288a5e3372f3456f97425b9
Jpopalonia <83743876+Jpopalonia@users.noreply.github.com>
2025-11-11 20:54:13 -0700

    Add files via upload

--- Zaun Viktor/code.py
+++ Zaun Viktor/code.py
@@ -1,1 +1,37 @@
+import time
+import board
+import pwmio
+import pwm_lightness
+PWM = pwm_lightness.get_pwm_table(0xffff, max_input=255)
 
+pin1 = pwmio.PWMOut(board.D3)
+pin2 = pwmio.PWMOut(board.D4)
+
+increasing1 = True
+increasing2 = True
+
+value1 = 90
+value2 = 230
+
+while True:
+    if value1 >= 255:
+        increasing1 = False
+    if value2 >= 255:
+        increasing2 = False
+    if value1 <= 32:
+        increasing1 = True
+    if value2 <= 32:
+        increasing2 = True
+    
+    if increasing1:
+        value1 += 1
+    if increasing2:
+        value2 += 1
+    if not increasing1:
+        value1 -= 1
+    if not increasing2:
+        value2 -= 1
+
+    pin1.duty_cycle = PWM[value1]
+    pin2.duty_cycle = PWM[value2]
+    time.sleep(0.02)
