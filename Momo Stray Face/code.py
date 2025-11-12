9f028e38044eb12a9b49e1385e4c6e9accfd1e11
Jpopalonia <83743876+Jpopalonia@users.noreply.github.com>
2025-11-11 20:11:30 -0700

    Create code.py

---
+++ Momo Stray Face/code.py
@@ -0,0 +1,25 @@
+# face driver code for momo cosplay
+
+# TODO
+# make good
+# use displayio to create interactive face
+# audio?
+# ???
+# profit!
+
+# module imports
+import board
+import displayio
+import sys
+
+# global variable initialization
+heck = 1
+
+# function definitions
+def butts():
+    print('ayo')
+
+# main loop
+while True:
+    butts()
+    sys.exit()
