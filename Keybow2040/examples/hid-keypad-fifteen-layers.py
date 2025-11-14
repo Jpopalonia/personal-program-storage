5a608f27f7e253478fd63cc49ed331ae6cb89078
Jpopalonia <83743876+Jpopalonia@users.noreply.github.com>
2025-11-14 00:10:26 -0700

    Add files via upload

--- Keybow2040/examples/hid-keypad-fifteen-layers.py
+++ Keybow2040/examples/hid-keypad-fifteen-layers.py
@@ -1,31 +1,29 @@
 # SPDX-FileCopyrightText: 2021 Sandy Macdonald
-#
+
+# Modified by Frank Smith 2021 all the keys bar the modifier key
+# can now be used as layer select and input keys
+# prints debug messages via debug serial port (USB)
+# sudo cat /dev/ttyACM0
+
 # SPDX-License-Identifier: MIT
 
 # An advanced example of how to set up a HID keyboard.
 
-# There are three layers, selected by pressing and holding key 0 (bottom left), 
-# then tapping one of the coloured layer selector keys above it to switch layer.
+# There are four layers defined out of fifteen possible,
+# selected by pressing and holding key 0 (bottom left), 
+# then tapping one of the coloured layer selector keys to switch layer.
 
-# The layer colours are as follows:
+# The defined layer colours are as follows:
 
 #  * layer 1: pink: numpad-style keys, 0-9, delete, and enter.
 #  * layer 2: blue: sends strings on each key press
-#  * layer 3: media controls, rev, play/pause, fwd on row one, vol. down, mute,
+#  * layer 3: yellow: media controls, rev, play/pause, fwd on row one, vol. down, mute,
 #             vol. up on row two
+#  * layer 4: white: sends mixxx controls
 
-# You'll need to connect Keybow 2040 to a computer, as you would with a regular
-# USB keyboard.
-
-# Drop the `keybow2040.py` file and `keybow_hardware` folder
-# into your `lib` folder on your `CIRCUITPY` drive.
-
-# NOTE! Requires the adafruit_hid CircuitPython library also!
-
+import board
 import time
 from keybow2040 import Keybow2040
-from keybow_hardware.pim56x import PIM56X as Hardware # for Keybow 2040
-#from keybow_hardware.pim551 import PIM551 as Hardware # for Pico RGB Keypad Base
 
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
@@ -36,7 +34,8 @@ from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 # Set up Keybow
-keybow = Keybow2040(Hardware())
+i2c = board.I2C()
+keybow = Keybow2040(i2c)
 keys = keybow.keys
 
 # Set up the keyboard and layout
@@ -49,9 +48,9 @@ consumer_control = ConsumerControl(usb_hid.devices)
 # Our layers. The key of item in the layer dictionary is the key number on
 # Keybow to map to, and the value is the key press to send.
 
-# Note that keys 0-3 are reserved as the modifier and layer selector keys 
-# respectively.
+# Note that key 0 is reserved as the modifier
 
+# purple - numeric keypad
 layer_1 =     {4: Keycode.ZERO,
                5: Keycode.ONE,
                6: Keycode.FOUR,
@@ -65,6 +64,7 @@ layer_1 =     {4: Keycode.ZERO,
                14: Keycode.SIX,
                15: Keycode.NINE}
 
+# blue - words
 layer_2 =     {7: "pack ",
                11: "my ",
                15: "box ",
@@ -74,6 +74,7 @@ layer_2 =     {7: "pack ",
                5: "liquor ",
                9: "jugs "}
 
+# yellow - media controls
 layer_3 =     {6: ConsumerControlCode.VOLUME_DECREMENT,
                7: ConsumerControlCode.SCAN_PREVIOUS_TRACK,
                10: ConsumerControlCode.MUTE,
@@ -81,16 +82,26 @@ layer_3 =     {6: ConsumerControlCode.VOLUME_DECREMENT,
                14: ConsumerControlCode.VOLUME_INCREMENT,
                15: ConsumerControlCode.SCAN_NEXT_TRACK}
 
+# white - mixxx
+layer_4 =     {2: Keycode.X,
+               5: Keycode.D,
+               7: Keycode.T,
+               8: Keycode.SPACE,
+               13: Keycode.L,
+               15: Keycode.Y}
+
 layers =      {1: layer_1,
                2: layer_2,
-               3: layer_3}
-
-# Define the modifier key and layer selector keys
-modifier = keys[0]
+               3: layer_3,
+               4: layer_4}
 
 selectors =   {1: keys[1],
                2: keys[2],
-               3: keys[3]}
+               3: keys[3],
+               4: keys[4]}
+
+# Define the modifier key and layer selector keys
+modifier = keys[0]
 
 # Start on layer 1
 current_layer = 1
@@ -98,55 +109,91 @@ current_layer = 1
 # The colours for each layer
 colours = {1: (255, 0, 255),
            2: (0, 255, 255),
-           3: (255, 255, 0)}
+           3: (255, 255, 0),
+           4: (128, 128, 128)}
 
-layer_keys = range(4, 16)
+layer_keys = range(0, 16)
+
+# dictionary of sets (sets cannot be changed but can be replaced)
+LEDs = {0: (64, 0, 0),
+        1: (128, 0, 0),
+        2: (196, 0, 0),
+        3: (255, 0, 0),
+        4: (0, 4, 0),
+        5: (0, 128, 0),
+        6: (0, 12, 0),
+        7: (0, 196, 0),
+        8: (0, 0, 64),
+        9: (0, 0, 128),
+        10: (0, 0, 196),
+        11: (0, 0, 255),
+        12: (64, 64, 0),
+        13: (128, 128, 0),
+        14: (196, 196, 0),
+        15: (255, 255, 0)}
 
 # Set the LEDs for each key in the current layer
 for k in layers[current_layer].keys():
     keys[k].set_led(*colours[current_layer])
 
+print("Starting!")
+mode = 0
+count = 0
+
 # To prevent the strings (as opposed to single key presses) that are sent from 
 # refiring on a single key press, the debounce time for the strings has to be 
 # longer.
+
 short_debounce = 0.03
 long_debounce = 0.15
 debounce = 0.03
 fired = False
 
 while True:
-    # Always remember to call keybow.update()!
+    # Always remember to call keybow.update()
     keybow.update()
 
-    # This handles the modifier and layer selector behaviour
+    # if no key is pressed ensure not locked in layer change mode
+    if ((mode == 2) & keybow.none_pressed()):
+        mode = 0
+        
     if modifier.held:
-        # Give some visual feedback for the modifier key
-        keys[0].led_off()
-
-        # If the modifier key is held, light up the layer selector keys
+        # set to looking to change the keypad layer
         for layer in layers.keys():
-            keys[layer].set_led(*colours[layer])
-
-            # Change layer if layer key is pressed
-            if current_layer != layer:
-                if selectors[layer].pressed:
+            # If the modifier key is held, light up the layer selector keys
+            if mode == 1:
+                print("Looking for layer select")
+                # Set the LEDs for each key in selectors
+                for k in layer_keys:
+                    keys[k].set_led(0, 0, 0)
+                for k in selectors.keys():
+                    keys[k].set_led(*colours[k])
+                keys[0].set_led(0, 255, 0)
+                mode = 2
+            
+            # Change current layer if layer key is pressed
+            if selectors[layer].pressed:
+                if mode >= 1:
+                    mode = 0
                     current_layer = layer
-
-                    # Set the key LEDs first to off, then to their layer colour
+                    print("Layer Changed:", current_layer)
+                    # Set the LEDs for each key in the current layer
                     for k in layer_keys:
                         keys[k].set_led(0, 0, 0)
-
-                    for k in layers[layer].keys():
-                        keys[k].set_led(*colours[layer])
-
-    # Turn off the layer selector LEDs if the modifier isn't held
+                    for k in layers[current_layer].keys():
+                        keys[k].set_led(*colours[current_layer])
     else:
-        for layer in layers.keys():
-            keys[layer].led_off()
+        # set to look for a key presses
+        if mode == 0:
+            print("Looking for key press on layer:", current_layer)
+            mode = 1
 
-        # Give some visual feedback for the modifier key
-        keys[0].set_led(0, 255, 25)
-
+            # Set the LEDs for each key in the current layer
+            for k in layer_keys:
+                keys[k].set_led(0, 0, 0)
+            for k in layers[current_layer].keys():
+                keys[k].set_led(*colours[current_layer])
+                
     # Loop through all of the keys in the layer and if they're pressed, get the
     # key code from the layer's key map
     for k in layers[current_layer].keys():
@@ -159,7 +206,10 @@ while True:
 
                 # Send the right sort of key press and set debounce for each
                 # layer accordingly (layer 2 needs a long debounce)
-                if current_layer == 1:
+                if (
+                        (current_layer == 1) |
+                        (current_layer == 4)
+                ):
                     debounce = short_debounce
                     keyboard.send(key_press)
                 elif current_layer == 2:
