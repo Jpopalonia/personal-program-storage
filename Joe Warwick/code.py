603d03d44e891c498fe86ac1833f15638b7bd75b
Jpopalonia <cmmartin24126@gmail.com>
2025-12-09 22:53:42 -0700

    code.py initial setup

--- Joe Warwick/code.py
+++ Joe Warwick/code.py
@@ -1,7 +1,30 @@
+# program to drive the LEDs and other devices in a full-body Warwick cosplay
+
+# program imports
 import board
 import time
 import random
 import neopixel
+import digitalio
 
+# animation lib imports
 from adafruit_led_animation.animation.pulse import Pulse
 
+# animation helper imports
+from adafruit_led_animation.helper import PixelSubset
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.group import AnimationGroup
+
+# color constant imports
+from adafruit_led_animation.color import *
+
+# global variable initialization
+rippy_dippy = True
+
+# function definitions
+def butts():
+    return 'my butt'
+
+# main loop
+while True:
+    pass
