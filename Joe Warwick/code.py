34b02a878c17a91591ddb6e4b098c5f5c89c9e9e
Jpopalonia <cmmartin24126@gmail.com>
2025-12-10 22:03:24 -0700

    Update code.py

--- Joe Warwick/code.py
+++ Joe Warwick/code.py
@@ -2,6 +2,8 @@
 
 # TODO:
 # make
+# control either over IR or BTLE!?!
+# clean up unused imports (they take a lot of memory)
 
 # program imports
 import board
@@ -11,13 +13,14 @@ import neopixel
 import digitalio
 
 # animation lib imports
-from adafruit_led_animation.animation.pulse import Pulse
+#from adafruit_led_animation.animation.pulse import Pulse
 from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 
 # animation helper imports
-from adafruit_led_animation.helper import PixelSubset
-from adafruit_led_animation.sequence import AnimationSequence
-from adafruit_led_animation.group import AnimationGroup
+#from adafruit_led_animation.helper import PixelSubset
+#from adafruit_led_animation.sequence import AnimationSequence
+#from adafruit_led_animation.group import AnimationGroup
 
 # color constant imports
 from adafruit_led_animation.color import *
@@ -25,7 +28,7 @@ from adafruit_led_animation.color import *
 # neopixel options and initialization
 neo_pin = board.NEOPIXEL
 num_pixels = 10
-neo_brightness = 1
+neo_brightness = 0.1
 
 pixels = neopixel.NeoPixel(
     pin = neo_pin,
@@ -35,17 +38,18 @@ pixels = neopixel.NeoPixel(
 )
 
 # animation options and initialization
-comet_speed = 0.1
-comet_color = BLACK
-comet_bg_color = GREEN
-comet_tail_length = 10
+comet_speed = 0.2
+comet_color = GREEN
+comet_bg_color = BLACK
+comet_tail_length = 13
 
 tube_animation = Comet(
     pixel_object = pixels,
     speed = comet_speed,
     color = comet_color,
     background_color = comet_bg_color,
-    tail_length = comet_tail_length
+    tail_length = comet_tail_length,
+    ring = True
 )
 
 # global variable initialization
