95832400ef864d0dc0c4c5764f5281018a72a07e
Jpopalonia <cmmartin24126@gmail.com>
2026-01-07 19:26:53 -0700

    Update code.py

--- Circuit Python Boilerplate/code.py
+++ Circuit Python Boilerplate/code.py
@@ -10,6 +10,18 @@ import random
 import neopixel
 import digitalio
 
+import supervisor
+
+# initialize built-in LED
+led = digitalio.DigitalInOut(board.LED)
+led.switch_to_output(value = False)
+
+# show whether USB is connected
+if supervisor.runtime.usb_connected:
+  led.value = True   # USB
+else:
+  led.value = False  # no USB
+
 # pin definitions
 
 # global variable initialization
